6cceec76b9f672fe5a1e4849a0a39a908f0032f0
feroshjacob <feroshjacob@gmail.com>
2014-12-17 11:08:03 -0500

    SBT windows changes

--- TailorSwiftPlugin/src/tailorswift/Activator.java
+++ TailorSwiftPlugin/src/tailorswift/Activator.java
@@ -1,5 +1,8 @@
 package tailorswift;
 
+import org.eclipse.core.internal.resources.File;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
@@ -11,6 +14,18 @@ public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "TailorSwiftPlugin"; //$NON-NLS-1$
+	
+
+	//The identifiers for the preferences	
+	public static final String SBT_HOME = "sbt_home";
+	public static final String SSH_CONNECTION_STRING = "ssh_connection_string";
+	public static final String SSH_PASSWORD = "ssh_password";
+
+	//The default values for the preferences
+	public static final String DEFAULT_SBT_HOME= "bug;bogus;hack;";
+	public static final String DEFAULT_SSH_CONNECTION_STRING = "username@hostname.com";
+	public static final String DEFAULT_SSH_PASSWORD = "password";
+
 
 	// The shared instance
 	private static Activator plugin;
@@ -57,5 +72,15 @@ public class Activator extends AbstractUIPlugin {
 	 */
 	public static ImageDescriptor getImageDescriptor(String path) {
 		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
+	protected void initializeDefaultPreferences(IPreferenceStore store) {
+		store.setDefault(SBT_HOME, DEFAULT_SBT_HOME);
+		store.setDefault(SSH_CONNECTION_STRING, DEFAULT_SSH_CONNECTION_STRING);
+		store.setDefault(SSH_PASSWORD, DEFAULT_SSH_PASSWORD);
+
+	}
+
+	public static String getSBTPath() {
+		return getDefault().getPreferenceStore().getString(SBT_HOME) ;
 	}
 }
