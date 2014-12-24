7334426ab83d2ff30e24feb7639ba7e5fe0414cb
feroshjacob <feroshjacob@gmail.com>
2014-12-24 00:40:50 -0500

    base class from JobsWithResult and

--- TailorSwiftPlugin/src/cb/tailorswift/behavior/ExecuteCommand.java
+++ TailorSwiftPlugin/src/cb/tailorswift/behavior/ExecuteCommand.java
@@ -4,13 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Plugin;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbench;
