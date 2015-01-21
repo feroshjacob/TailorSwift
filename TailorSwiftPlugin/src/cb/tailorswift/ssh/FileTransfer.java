4850d0ba5097adcb6987e86c949cce8c72c07cef
feroshjacob <feroshjacob@gmail.com>
2015-01-21 04:07:29 -0500

    File transfer with monitoring based on size

--- TailorSwiftPlugin/src/cb/tailorswift/ssh/FileTransfer.java
+++ TailorSwiftPlugin/src/cb/tailorswift/ssh/FileTransfer.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+
 import com.jcraft.jsch.Channel;
 import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSch;
@@ -32,98 +34,120 @@ public class FileTransfer {
 		session.setUserInfo(ui);
 		session.connect();
 	}
-	public File transferFromServer(  String serverPath, String  localFilePath) throws IOException,
-	JSchException {
-		  FileOutputStream fos=null;
-		  String rfile = serverPath;
-		  String lfile = localFilePath;
-		  String command="scp -f "+rfile;
-	      Channel channel=session.openChannel("exec");
-	      ((ChannelExec)channel).setCommand(command);
 
-	      // get I/O streams for remote scp
-	      OutputStream out=channel.getOutputStream();
-	      InputStream in=channel.getInputStream();
+	public File transferFromServer(String serverPath, String localFilePath)
+			throws IOException, JSchException {
+		FileOutputStream fos = null;
+		String rfile = serverPath;
+		String lfile = localFilePath;
+		String command = "scp -f " + rfile;
+		Channel channel = session.openChannel("exec");
+		((ChannelExec) channel).setCommand(command);
 
-	      channel.connect();
+		// get I/O streams for remote scp
+		OutputStream out = channel.getOutputStream();
+		InputStream in = channel.getInputStream();
 
-	      byte[] buf=new byte[1024];
+		channel.connect();
 
-	      // send '\0'
-	      buf[0]=0; out.write(buf, 0, 1); out.flush();
+		byte[] buf = new byte[1024];
 
-	      while(true){
-		int c=checkAck(in);
-	        if(c!='C'){
-		  break;
+		// send '\0'
+		buf[0] = 0;
+		out.write(buf, 0, 1);
+		out.flush();
+
+		while (true) {
+			int c = checkAck(in);
+			if (c != 'C') {
+				break;
+			}
+
+			// read '0644 '
+			in.read(buf, 0, 5);
+
+			long filesize = 0L;
+			while (true) {
+				if (in.read(buf, 0, 1) < 0) {
+					// error
+					break;
+				}
+				if (buf[0] == ' ')
+					break;
+				filesize = filesize * 10L + (long) (buf[0] - '0');
+			}
+
+			@SuppressWarnings("unused")
+			String file = null;
+			for (int i = 0;; i++) {
+				in.read(buf, i, 1);
+				if (buf[i] == (byte) 0x0a) {
+					file = new String(buf, 0, i);
+					break;
+				}
+			}
+
+			// System.out.println("filesize="+filesize+", file="+file);
+
+			// send '\0'
+			buf[0] = 0;
+			out.write(buf, 0, 1);
+			out.flush();
+
+			// read a content of lfile
+			fos = new FileOutputStream(lfile);
+			int foo;
+			while (true) {
+				if (buf.length < filesize)
+					foo = buf.length;
+				else
+					foo = (int) filesize;
+				foo = in.read(buf, 0, foo);
+				if (foo < 0) {
+					// error
+					break;
+				}
+				fos.write(buf, 0, foo);
+				filesize -= foo;
+				if (filesize == 0L)
+					break;
+			}
+			fos.close();
+			fos = null;
+
+			if (checkAck(in) != 0) {
+				System.exit(0);
+			}
+
+			// send '\0'
+			buf[0] = 0;
+			out.write(buf, 0, 1);
+			out.flush();
 		}
-
-	        // read '0644 '
-	        in.read(buf, 0, 5);
-
-	        long filesize=0L;
-	        while(true){
-	          if(in.read(buf, 0, 1)<0){
-	            // error
-	            break; 
-	          }
-	          if(buf[0]==' ')break;
-	          filesize=filesize*10L+(long)(buf[0]-'0');
-	        }
-
-	        @SuppressWarnings("unused")
-			String file=null;
-	        for(int i=0;;i++){
-	          in.read(buf, i, 1);
-	          if(buf[i]==(byte)0x0a){
-	            file=new String(buf, 0, i);
-	            break;
-	  	  }
-	        }
-
-		//System.out.println("filesize="+filesize+", file="+file);
-
-	        // send '\0'
-	        buf[0]=0; out.write(buf, 0, 1); out.flush();
-
-	        // read a content of lfile
-	        fos=new FileOutputStream(lfile);
-	        int foo;
-	        while(true){
-	          if(buf.length<filesize) foo=buf.length;
-		  else foo=(int)filesize;
-	          foo=in.read(buf, 0, foo);
-	          if(foo<0){
-	            // error 
-	            break;
-	          }
-	          fos.write(buf, 0, foo);
-	          filesize-=foo;
-	          if(filesize==0L) break;
-	        }
-	        fos.close();
-	        fos=null;
-
-		if(checkAck(in)!=0){
-		  System.exit(0);
-		}
-
-	        // send '\0'
-	        buf[0]=0; out.write(buf, 0, 1); out.flush();
-	      }
-	   //   new File()
-	  	channel.disconnect();
-	      session.disconnect();
-	      return new File(lfile);
+		// new File()
+		channel.disconnect();
+		session.disconnect();
+		return new File(lfile);
 
 	}
 
 	public void transferToServer(File file, String outFile) throws IOException,
+	JSchException {
+		transferToServer(file, outFile, null);
+	}
+	public void transferToServer(File file, String outFile, IProgressMonitor monitor) throws IOException,
 			JSchException {
 		FileInputStream fis = null;
 
 		boolean ptimestamp = true;
+		long filesize = file.length();
 
+		byte[] buf = new byte[2];
+		int blocks = (int)(filesize + buf.length-1)/ buf.length;
+		
+		if(monitor!=null)
+		monitor.beginTask("Uploading file: "+ file.getName() ,blocks+2);
+		
 		// exec 'scp -t rfile' remotely
 		String command = "scp " + (ptimestamp ? "-p" : "") + " -t " + outFile;
 		Channel channel = session.openChannel("exec");
@@ -138,6 +162,7 @@ public class FileTransfer {
 		if (checkAck(in) != 0) {
 			return;
 		}
+		
 
 		if (ptimestamp) {
 			command = "T " + (file.lastModified() / 1000) + " 0";
@@ -150,9 +175,11 @@ public class FileTransfer {
 				return;
 			}
 		}
+		if(monitor!=null)
+			monitor.worked(1);
 
 		// send "C0644 filesize filename", where filename should not include '/'
-		long filesize = file.length();
+		
 		command = "C0644 " + filesize + " ";
 
 		command += file.getName();
@@ -163,14 +190,22 @@ public class FileTransfer {
 			return;
 		}
 
+		if(monitor!=null)
+			monitor.worked(1);
+		
 		// send a content of lfile
 		fis = new FileInputStream(file);
-		byte[] buf = new byte[1024];
+
+	  
 		while (true) {
+			
 			int len = fis.read(buf, 0, buf.length);
 			if (len <= 0)
 				break;
 			out.write(buf, 0, len); // out.flush();
+			if(monitor!=null)
+				monitor.worked(1);
+		
 		}
 		fis.close();
 		fis = null;
