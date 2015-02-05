7f6a2717cfbf7b9752f86797ac5d930db60b0577
feroshjacob <feroshjacob@gmail.com>
2015-02-04 23:30:19 -0500

    Split into two for future changes

--- TailorSwiftPlugin/src/com/recipegrace/tailorswift/launch/LaunchSBTWebScalding.java
+++ TailorSwiftPlugin/src/com/recipegrace/tailorswift/launch/LaunchSBTWebScalding.java
@@ -1,5 +1,7 @@
 package com.recipegrace.tailorswift.launch;
 
+import static com.recipegrace.tailorswift.launch.ui.WebScaldingLaunchTab.WEBSCALDING_LAUNCH_SKIP_BUILD;
+
 import java.net.MalformedURLException;
 
 import org.eclipse.core.resources.IProject;
@@ -7,6 +9,7 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
@@ -18,54 +21,72 @@ import com.recipegrace.tailorswift.launch.ui.WebScaldingLaunchTab;
 import com.recipegrace.tailorswift.newproject.SBTProjectSupport;
 
 public class LaunchSBTWebScalding extends JavaLaunchDelegate {
-	
+
 	@Override
 	public void launch(ILaunchConfiguration configuration, String mode,
 			ILaunch launch, IProgressMonitor monitor) throws CoreException {
+		boolean skipBuild = configuration.getAttribute(
+				WEBSCALDING_LAUNCH_SKIP_BUILD, false);
 		try {
-			ILaunchConfiguration config = SBTProjectSupport.setSBTSpecificParms(
-					configuration);
-			super.launch(config, mode, launch, monitor);
+			ILaunchConfiguration config = SBTProjectSupport
+					.setSBTSpecificParms(configuration);
+
+			if (!skipBuild){
+				super.launch(config, mode, launch, monitor);
+				ILaunchesListener2 launchListener = new WebScaldingBuildTerminateListener(
+						configuration,false);
+				DebugPlugin.getDefault().getLaunchManager()
+				.addLaunches(new ILaunch[] { launch });
+				DebugPlugin.getDefault().getLaunchManager()
+				.addLaunchListener(launchListener);
+			}else {
+				Job job = new PostWebScaldingBuildTask(configuration,"Submitting Scalding Job", false);
+				job.schedule();
+
+			}
+			
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			new IOUtils().logError(e, "launch failed");
 		}
-		ILaunchesListener2 launchListener = new WebScaldingBuildTerminateListener(configuration); 
-        DebugPlugin.getDefault().getLaunchManager().addLaunches(new ILaunch[] {launch});
-        DebugPlugin.getDefault().getLaunchManager().addLaunchListener(launchListener);
-        
-	
-		
+
+
+		if (skipBuild)
+			launch.terminate();
+
 	}
 
-
-	
 	@Override
 	public String getMainTypeName(ILaunchConfiguration configuration)
 			throws CoreException {
 		return "xsbt.boot.Boot";
 	}
-	
+
 	@Override
 	public String getProgramArguments(ILaunchConfiguration configuration)
 			throws CoreException {
 		return "clean assembly";
 	}
+
 	@Override
 	public String getVMArguments(ILaunchConfiguration configuration)
 			throws CoreException {
-		StringBuilder sb = new StringBuilder(super.getVMArguments(configuration) );
-		
+		StringBuilder sb = new StringBuilder(
+				super.getVMArguments(configuration));
+
 		sb.append(" -Djline.WindowsTerminal.directConsole=false ");
 		sb.append("-Dsbt.log.noformat=true ");
 		return sb.toString();
 	}
+
 	@Override
 	public IPath getWorkingDirectoryPath(ILaunchConfiguration configuration)
 			throws CoreException {
-	    String projectName= configuration.getAttribute(WebScaldingLaunchTab.WEBSCALDING_LAUNCH_PROJECT_NAME, ""); 
-	    IProject project=  ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
-	    return project.getLocation();
+		String projectName = configuration.getAttribute(
+				WebScaldingLaunchTab.WEBSCALDING_LAUNCH_PROJECT_NAME, "");
+		IProject project = ResourcesPlugin.getWorkspace().getRoot()
+				.getProject(projectName);
+		return project.getLocation();
 	}
-		
+
 }
